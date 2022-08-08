a867e55175eb83c5930e295b08f3005a1e051b0f
PirateX20 <feliperangelm16dsm@gmail.com>
2022-08-08 11:24:51 -0300

    edit privilegio

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import RealImage from "./ImageAcess/RealImage";
 //cria imagens teste
 let imagem1 = new ProxyImage("../Images/teste.jpg");
 let imagem2 =  new ProxyImage("../Images/abracadabra.png");
+//Acessa o arquivo real diretamente
+let imagem3 = new RealImage("../Imagens/privilegio.jpg");
+console.log("\n");
 imagem1.exibicao();
 console.log("\n");
 imagem2.exibicao(); 
+console.log("\n");
